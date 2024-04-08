34bc8c1c93f52ff04ac20dbf3aaca9e44266377c
code-faiza-1998 <faiza5198@gmail.com>
2024-04-08 04:11:13 -0700

    CLI based currency converter by using typescript,node.js, inquirer and chalk

---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+const currency : any = {
+    USD : 1 ,
+    EUR : 0.91,
+    GBP : 0.76,
+    INR : 74.57,
+    PKR : 280,
+}
+let UserAnswer=  await inquirer.prompt([{
+
+    name:"from",
+    message:(chalk.greenBright("Enter from currency")),
+    type:"list",
+    choices:["USD", "EUR", "GBP", "INR", "PKR"],
+},
+{
+
+    name:"to",
+    message:(chalk.greenBright("Enter to currency")),
+    type:"list",
+    choices:["USD", "EUR", "GBP", "INR", "PKR"]
+},
+{
+
+    name:"amount",
+    message:(chalk.greenBright("Enter your amount")),
+    type:"number",
+}
+])
+let fromAmount = currency[UserAnswer.from];
+let toAmount = currency[UserAnswer.to];
+let amount = UserAnswer.amount;
+let baseAmount = amount/fromAmount;
+let convertedAmount = baseAmount*toAmount;
+console.log(convertedAmount.toFixed());
